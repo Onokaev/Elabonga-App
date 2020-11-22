39cf830cc0f55198fb92daa3dc157e341782bdfb
onokaev <evansonoka05.j@gmail.com>
2020-11-22 13:31:25 +0300

    ela a

---
+++ app.py
@@ -0,0 +1,42 @@
+# app.py
+
+# Required imports
+import os
+from flask import Flask, request, jsonify
+from firebase_admin import credentials, firestore, initialize_app
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Initialize Firestore DB
+cred = credentials.Certificate('key.json')
+default_app = initialize_app(cred)
+db = firestore.client()
+todo_ref = db.collection('todos')
+
+
+@app.route('/update/', methods=['POST']) #'POST', 'PUT'
+def update():
+    """
+        update() : Update document in Firestore collection with request body.
+        Ensure you pass a custom IsD as part of json body in post request,
+        e.g. json={'id': '1', 'title': 'Write a blog post today'}
+    """
+    name = "" #projects/munchies-a2feb/databases/(default)/documents/foodEaten/zskIV2hw6vmxnH3RPJXg
+    fields = ""
+    rfidTag = "D4078AC3"
+    amount = 0
+
+    doc_ref = db.collection(u'foodEaten').document(u'zskIV2hw6vmxnH3RPJXg')
+    doc_ref.set({
+        #u'name': name,
+        u'rfidTag': rfidTag,
+        u'amount': amount    
+
+    })
+
+    return "Hello world"
+
+port = int(os.environ.get('PORT', 8080))
+if __name__ == '__main__':
+    app.run(threaded=True, host='0.0.0.0', port=port)
